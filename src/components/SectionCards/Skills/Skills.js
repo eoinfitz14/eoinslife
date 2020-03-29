9d72547c29e2c71f3399bf8168a99ddae178de47
eoinfitz14 <eoin.fitzsimons14@gmail.com>
2020-03-29 20:15:05 +0100

    fixed skills list

--- src/components/SectionCards/Skills/Skills.js
+++ src/components/SectionCards/Skills/Skills.js
@@ -6,16 +6,18 @@ import './Skills.css';
 const Skills = () => {
   return (
     <div className='skills-bg pv5 avenir'>
-    <article className='center blur w-70 pa5 hidden br3 mv4 shadow-3'>
-      <h1 className='f4 br3 br--top black mv0 pv2 ph3 baskerville'>
-        SKILLS
+      <article className='center blur w-70 pa5 hidden br3 mv4 shadow-3'>
+        <h1 className='f4 br3 br--top black mv0 pv2 ph3 baskerville'>
+          SKILLS
         </h1>
         <div className='center tl list p10 '>
+          <h4>Proven Technical Abilities</h4>
           <ul className='ul4'>
             <li>Java</li>
             <li>NodeJS</li>
             <li>ReactJS</li>
             <li>ExpressJS</li>
+            <li>C</li>
             <li>C++</li>
             <li>OpenGL</li>
             <li>MySQL</li>
