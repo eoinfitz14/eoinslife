5b9b97abd18f4c18ba6e6fd8791e2b8b40f06c0d
eoinfitz14 <eoin.fitzsimons14@gmail.com>
2020-04-10 22:22:18 +0100

    added experie

--- src/components/SectionCards/Experience/Experience.js
+++ src/components/SectionCards/Experience/Experience.js
@@ -17,10 +17,21 @@ const Experience = () => {
               September 2019 - Present
             </span>
             <br></br>
-            <span className='pv3 fl no-underline'>
-            
-            </span>
           </p>
+          <ul className='tl'>
+            <li>Working on mapview. explain product</li>
+            <li>Writing automated tests using Java and Cucumber BDD</li>
+            <li>Ensuring quality in testing as well as Jira tickets</li>
+            <li>Training in interns</li>
+            <li>Designing online technical assessments for future candidates</li>
+            <li>Reviewing CVs and Interviews (????)</li>
+            <li>Managing testing of X amount of clients</li>
+            <li>Sharing work with other Quality Engineers from project the I wrote</li>
+            <li>Privacy Champion with LNPD</li>
+            <li>Assist other teams with starting their automation frameworks (platform)</li>
+            <li>Reliability - answering emergency calls out of hours</li>
+            <li>Assisting the team in getting one step closer to CI/CD</li>
+          </ul>
         </div>
         <div >
           <p className='pt1'>
@@ -31,10 +42,24 @@ const Experience = () => {
               January 2019 - July 2019
             </span>
             <br></br>
-            <span className='pv3 fl no-underline'>
-            
-            </span>
           </p>
+          <ul className='tl'>
+            <li>Learned Java for the first time</li>
+            <li>Revival of automated testing</li>
+            <li>Learned agile methodologies and BDD</li>
+            <li>Took every opportunity to learn something new</li>
+            <li>Working on mapview</li>
+            <li>Testing was not something that we learned in college. 
+                This taught me about the fine detail in delivering a quality product, not just part of one
+            </li>
+            <li>After impressing in the first half of internship, a well-timed opportunity came about
+                A senior engineer was leaving the company just as an integration project between two major products was starting.
+                I had sunk my teeth into a lot of test automation at this point and took her place in the small team of 5.
+            </li>
+            <li>Wrote automation project from scratch for new product</li>
+            <li>Got to work on the project from the 1st day of implementation, finishing it off as a full-time employee</li>
+            <li>Three amigos sessions - working with dev and BAs</li>
+          </ul>
         </div>
         <div >
           <p className='pt1'>
--- src/components/SectionCards/Skills/Skills.js
+++ src/components/SectionCards/Skills/Skills.js
@@ -13,7 +13,7 @@ const Skills = () => {
         <div className='tl underline pv3'>
             Proven Technical Skills:
         </div>
-        <div className='center tl list p10 '>
+        <div className=' tl list p10 '>
           <ul id='computerList' className='ul4'>
             <li>Java</li>
             <li>NodeJS</li>
@@ -35,6 +35,25 @@ const Skills = () => {
             <li>Scrum</li>
             <li>Jira</li>
             <li>Confluence</li>
+          </ul>
+        </div>
+        <div className='tl underline pv3'>
+            Proven Soft Skills:
+        </div>
+        <div className='tl list p10 '>
+          <p className='mb4'>
+             From the time I started college, I have not stopped adding new things to my technical arsenal. However, there are
+             certain soft skills that I have been improving since a young age.
+          </p>
+          <ul id='checkList' className='ul4'>
+            <li>Leadership</li>
+            <li>Teamwork</li>
+            <li>Always Learning</li>
+            <li>Problem Solving</li>
+            <li>Strong Work Ethic</li>
+            <li>Organisation</li>
+            <li>Actively Listening</li>
+            <li>Adapt Quickly To New Environments</li>
           </ul>
         </div>
         <div className='tl underline pv3'>
