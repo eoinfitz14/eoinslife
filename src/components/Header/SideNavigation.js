6474562060097c26f45ae1899cd031f35d763407
eoinfitz14 <eoin.fitzsimons14@gmail.com>
2020-07-15 17:00:23 +0100

    fixed sidenav bar

--- src/components/Header/SideNavigation.js
+++ src/components/Header/SideNavigation.js
@@ -71,8 +71,9 @@ class SideNavigation extends Component{
             <nav >
               <ul className='side-nav shadow-3'>
                 <Link to='about'      spy={true} smooth={true} duration={750} activeClass='active' className='link bg-animate black-80 hover-bg-lightest-blue dib pv3 ph4 pointer'>About</Link>
+                <Link to='experience' spy={true} smooth={true} duration={750} activeClass='active' className='link bg-animate black-80 hover-bg-lightest-blue dib pv3 ph4 pointer'>Experience</Link>
+                <Link to='projects'   spy={true} smooth={true} duration={750} activeClass='active' className='link bg-animate black-80 hover-bg-lightest-blue dib pv3 ph4 pointer'>Projects</Link>
                 <Link to='education'  spy={true} smooth={true} duration={750} activeClass='active' className='link bg-animate black-80 hover-bg-lightest-blue dib pv3 ph4 pointer'>Education</Link>
-                <Link to='experience' spy={true} smooth={true} duration={750} activeClass='active' className='link bg-animate black-80 hover-bg-lightest-blue dib pv3 ph4 pointer'>Work Experience</Link>
                 <Link to='skills'     spy={true} smooth={true} duration={750} activeClass='active' className='link bg-animate black-80 hover-bg-lightest-blue dib pv3 ph4 pointer'>Skills</Link>
                 <Link to='awards'     spy={true} smooth={true} duration={750} activeClass='active' className='link bg-animate black-80 hover-bg-lightest-blue dib pv3 ph4 pointer'>Awards</Link>
               </ul>
